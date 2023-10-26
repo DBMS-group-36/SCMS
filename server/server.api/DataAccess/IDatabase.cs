74334923a31a61f4076af0f1c053e80cf7b3cee5
Tharindu Perera <tharindu6516@outlook.com>
2023-10-26 11:43:02 +0530

    Setup api  - Connect to database  - Integration with Identity for authentication  - Itegration with sendGrid for email  - Setup gRPC and REST api for authentication (Regiser and Login)

---
+++ server/server.api/DataAccess/IDatabase.cs
@@ -0,0 +1,9 @@
+﻿namespace server.api.DataAccess;
+
+public interface IDatabase
+{
+    public Task<IEnumerable<T>> QueryAllAsync<T>(string sql, params object[] args);
+    public Task<T> QueryFirstAsync<T>(string sql, params object[] args);
+    public Task<int> ExecuteAsync(string sql, params object[] args);
+    public Task<T> ExecuteScalarAsync<T>(string sql, params object[] args);
+}
---
+++ server/server.api/DataAccess/SqlQueryExtensions/ToSqlString.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace server.api.DataAccess.SqlQueryExtensions;
+
+public static class ToSqlString
+{
+    public static string toSqlString(this string s)
+    {
+        if (s == null) return "null";
+        var ret = Regex.Replace(s, @"\\", @"\\");
+        ret = Regex.Replace(ret, @"'", @"\'");
+        return $"'{ret}'";
+    }
+
+    public static string toSqlString(this int? i)
+    {
+        if (i == null) return "null";
+        return i.ToString();
+    }
+
+    public static string toSqlString(this int i)
+    {
+        return i.ToString();
+    }
+
+    public static string toSqlString(this bool? b)
+    {
+        if (b == null) return "null";
+        return b.ToString();
+    }
+
+    public static string toSqlString(this bool b)
+    {
+        return b.ToString();
+    }
+
+    public static string toSqlString(this DateTimeOffset? d)
+    {
+        if (d == null) return "null";
+        return d.Value.ToString("'yyyy-MM-dd HH-mm-ss'");
+    }
+}
