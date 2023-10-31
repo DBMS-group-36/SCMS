078dbb85aea0e09c8ac19981e5869528314cce9e
nipunadulara <flangodev@gmail.com>
2023-10-31 20:59:26 +0530

    add order

--- server/server.api/gRPC/Services/Customer/OrderService.cs
+++ server/server.api/gRPC/Services/Customer/OrderService.cs
@@ -15,7 +15,7 @@ using System.Configuration;
 
 namespace server.api.gRPC.Services.Customer;
 
-[Authorize]
+
 public class OrderService : Order.OrderBase
 {
     private readonly IDatabase database;
@@ -59,6 +59,32 @@ public class OrderService : Order.OrderBase
 
         reply.Count = await database.ExecuteScalarAsync<long>(countSql);
 
+        return reply;
+    }
+    public async override Task<PlaceOrderReply> PlaceOrder(PlaceOrderRequest request, ServerCallContext context)
+    {
+
+        Console.WriteLine(request);
+        Console.WriteLine(request.OrderItems);
+        var reply = new PlaceOrderReply();
+
+
+
+        var sql = $"INSERT INTO orders (OrderDate, DeliveryDate, DeliveryAddressId, RouteId, OrderCapacity,  price, StoreId) VALUES ({request.OrderDate.ToSqlString()}, {request.DeliveryDate.ToSqlString()}, {request.DeliveryAddressId.ToSqlString()}, {request.RouteId}, {request.OrderCapacity}, {request.Price}, {request.StoreId});";
+
+
+
+
+         await database.QueryAllAsync<ListedProductMessage>(sql);
+
+        reply.OrderId = await database.ExecuteScalarAsync<ulong>("SELECT LAST_INSERT_ID();");
+
+        foreach (OrderItem i in request.OrderItems)
+        {
+            sql = $"INSERT INTO order_products (OrderId, ProductId, Quantity,UnitPrice) VALUES ({reply.OrderId}, {i.ItemId}, {i.Quantity}, {i.UnitPrice});";
+             await database.QueryAllAsync<ListedProductMessage>(sql);
+        }
+        await database.QueryAllAsync<ListedProductMessage>("commit;");
         return reply;
     }
 }
